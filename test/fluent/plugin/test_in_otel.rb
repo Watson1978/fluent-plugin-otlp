bd2a9bf79a9009a8ee06c6a8389ccc141477ada2
Shizuo Fujita <fujita@clear-code.com>
2025-04-10 14:55:17 +0900

    add

--- test/fluent/plugin/test_in_otel.rb
+++ test/fluent/plugin/test_in_otel.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 require "helper"
-require 'fluent/test/driver/input'
-require 'fluent/plugin/in_otlp'
-require 'excon'
-require 'timecop'
+
+require "fluent/plugin/in_otlp"
+require "fluent/test/driver/input"
 
 class Fluent::Plugin::OtlpInputTest < Test::Unit::TestCase
   def config
@@ -21,11 +20,7 @@ class Fluent::Plugin::OtlpInputTest < Test::Unit::TestCase
     Fluent::Test.setup
   end
 
-  def teardown
-    Timecop.return
-  end
-
-  def create_driver(conf=config)
+  def create_driver(conf = config)
     Fluent::Test::Driver::Input.new(Fluent::Plugin::OtlpInput).configure(conf)
   end
 
@@ -36,23 +31,103 @@ class Fluent::Plugin::OtlpInputTest < Test::Unit::TestCase
     assert_equal 4318, d.instance.http_config.port
   end
 
-  def test_receive_json
+  data("metrics" => {
+         request_path: "/v1/metrics",
+         request_data: TestData::JSON::METRICS,
+         record_type: "otlp_metrics",
+         record_data: TestData::JSON::METRICS
+       },
+       "traces" => {
+         request_path: "/v1/traces",
+         request_data: TestData::JSON::TRACES,
+         record_type: "otlp_traces",
+         record_data: TestData::JSON::TRACES
+       },
+       "logs" => {
+         request_path: "/v1/logs",
+         request_data: TestData::JSON::LOGS,
+         record_type: "otlp_logs",
+         record_data: TestData::JSON::LOGS
+       })
+  def test_receive_json(data)
     d = create_driver
     res = d.run(expect_records: 1) do
-      post_json("/v1/metrics", Fluent::Plugin::Otlp::JSON::METRICS)
+      post_json(data[:request_path], data[:request_data])
     end
 
     assert_equal(200, res.status)
     assert_equal("otlp.test", d.events[0][0])
-    assert_equal({ type: "otlp_metrics", message: Fluent::Plugin::Otlp::JSON::METRICS }, d.events[0][2])
+    assert_equal({ type: data[:record_type], message: data[:record_data] }, d.events[0][2])
   end
 
-  def post_json(path, json)
-    headers = { "Content-Type" => "application/json" }
-    post(path, headers, json)
+  def test_receive_compressed_json
+    d = create_driver
+    res = d.run(expect_records: 1) do
+      post_json("/v1/metrics", compress(TestData::JSON::METRICS), { "Content-Encoding" => "gzip" })
+    end
+
+    assert_equal(200, res.status)
+    assert_equal("otlp.test", d.events[0][0])
+    assert_equal({ type: "otlp_metrics", message: TestData::JSON::METRICS }, d.events[0][2])
   end
 
-  def post(path, headers, body)
+  data("metrics" => {
+         request_path: "/v1/metrics",
+         request_data: TestData::ProtocolBuffers::METRICS,
+         record_type: "otlp_metrics",
+         record_data: TestData::JSON::METRICS
+       },
+       "traces" => {
+         request_path: "/v1/traces",
+         request_data: TestData::ProtocolBuffers::TRACES,
+         record_type: "otlp_traces",
+         record_data: TestData::JSON::TRACES
+       },
+       "logs" => {
+         request_path: "/v1/logs",
+         request_data: TestData::ProtocolBuffers::LOGS,
+         record_type: "otlp_logs",
+         record_data: TestData::JSON::LOGS
+       })
+  def test_receive_protocol_buffers(data)
+    d = create_driver
+    res = d.run(expect_records: 1) do
+      post_protobuf(data[:request_path], data[:request_data])
+    end
+
+    assert_equal(200, res.status)
+    assert_equal("otlp.test", d.events[0][0])
+    assert_equal({ type: data[:record_type], message: data[:record_data] }, d.events[0][2])
+  end
+
+  def test_receive_compressed_protocol_buffers
+    d = create_driver
+    res = d.run(expect_records: 1) do
+      post_json("/v1/metrics", compress(TestData::ProtocolBuffers::METRICS), { "Content-Encoding" => "gzip" })
+    end
+
+    assert_equal(200, res.status)
+    assert_equal("otlp.test", d.events[0][0])
+    assert_equal({ type: "otlp_metrics", message: TestData::JSON::METRICS }, d.events[0][2])
+  end
+
+  def compress(data)
+    gz = Zlib::GzipWriter.new(StringIO.new)
+    gz << data
+    gz.close.string
+  end
+
+  def post_json(path, json, headers = {})
+    headers = headers.merge({ "Content-Type" => "application/json" })
+    post(path, json, headers)
+  end
+
+  def post_protobuf(path, binary, headers = {})
+    headers = headers.merge({ "Content-Type" => "application/x-protobuf" })
+    post(path, binary, headers)
+  end
+
+  def post(path, body, headers = {})
     connection = Excon.new("http://127.0.0.1:4318#{path}", body: body, headers: headers)
     connection.post
   end
--- test/helper.rb
+++ test/helper.rb
@@ -2,17 +2,29 @@
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
+require "fluent/plugin/otlp/request"
+require "fluent/test"
+require "fluent/test/helpers"
+
+require "excon"
+require "json"
 require "test-unit"
-require 'fluent/test'
-require 'fluent/test/helpers'
-require 'json'
+require "timecop"
+require "zlib"
 
 include Fluent::Test::Helpers
 
-module Fluent::Plugin::Otlp
+module TestData
   module JSON
     # trim white spaces
     METRICS = ::JSON.generate(::JSON.parse(File.read(File.join(__dir__, "./fluent/assets/metrics.json"))))
-    TRACES = ::JSON.parse(File.read(File.join(__dir__, "./fluent/assets/traces")))
+    TRACES = ::JSON.generate(::JSON.parse(File.read(File.join(__dir__, "./fluent/assets/traces.json"))))
+    LOGS = ::JSON.generate(::JSON.parse(File.read(File.join(__dir__, "./fluent/assets/logs.json"))))
+  end
+
+  module ProtocolBuffers
+    METRICS = Fluent::Plugin::Otlp::Request::Metrics.new(TestData::JSON::METRICS).encode
+    TRACES = Fluent::Plugin::Otlp::Request::Traces.new(TestData::JSON::TRACES).encode
+    LOGS = Fluent::Plugin::Otlp::Request::Logs.new(TestData::JSON::LOGS).encode
   end
 end
